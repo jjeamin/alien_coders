db824287ed6999e58f683a2fb815af2af1520712
jjeamin <woalsdl600@gmail.com>
2023-08-03 05:59:16 +0900

    Update contents

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -35,7 +35,7 @@ def get_secret(setting, secrets=secrets):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.pythonanywhere.com']
 
