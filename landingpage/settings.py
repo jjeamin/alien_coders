a90384b84947e52a0ce3bf7c7a6d1ce56ffc6e2f
jjeamin <woalsdl600@gmail.com>
2023-08-03 04:48:34 +0900

    fix letter

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '.pythonanywhere.com']]
+ALLOWED_HOSTS = ['*', '.pythonanywhere.com']
 
 
 # Application definition
